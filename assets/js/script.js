b12c733011872d58f5d002d9719a819d811b7d6e
Roomsie Jones <seangravito@gmail.com>
2021-06-22 15:48:28 -0500

    added some of the rendering and changed some of my classes and IDs in html to callback later "

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,76 @@
 // Need header to start with "coding Quiz Challenge"
 // Need instructions
 // need start quiz button
-var questionEl = document.querySelector("h1")
+var questionEl = document.querySelector("h1");
+const start = document.getElementById('start');
+const quiz = document.getElementById('quiz');
+const newquestion = document.getElementById('question');
+
+const choiceA = document.getElementById('A');
+const choiceB = document.getElementById('B');
+const choiceC = document.getElementById('C');
+const choiceD = document.getElementById('D');
 
 // Need questions to replace the Headers
-// Question
+// This is my array of objects containing my questions, answers, and correct answers.
 var questions = [
-  {
+  
+    {
     question: "What is the best animal?",
     choices: ["Dog", "Cat", "Lion", "Pig"],
-    correct: "Cat",
-  },
-  {},
+    correct: "B",
+    },
+
+    {
+      question: "This is a different question?"
+      choices: ["___", "____", "____", "____"],
+      correct: "____",
+    },
+    
+    {
+    question: "This is a different question?"
+    choices: ["___", "____", "____", "____"],
+    correct: "____",
+    },
+    
+    {
+    question: "This is a different question?"
+    choices: ["___", "____", "____", "____"],
+    correct: "____",
+    },
+    
+    {
+    question: "This is a different question?"
+    choices: ["___", "____", "____", "____"],
+    correct: "____",
+    },
+
 ];
 
-console.log(questions[0].choices[1])
+let lastQuestionIndex = questions.length -1;
+let runningQuestionIndex = 0;
+
+// putting the questions and answers into html starting with the first question
+function renderQuestion(){
+    let q = questions[runningQuestionIndex];
+    newquestion.innerHTML = "<p>" + q.question + "</p>"
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
+
+}
+
+// starts with first question
+runningQuestionIndex = 0;
+renderQuestion ();
+
+// progresses to next question
+runningQuestionIndex++;
+renderQuestion();
+
+
+
 
 questionEl.textContent = questions[0].question;
 // 4 answers
