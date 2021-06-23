7a2187089d9cd2a2dd2e88cdf29fd3ae3fe594c8
Roomsie Jones <seangravito@gmail.com>
2021-06-22 21:15:26 -0500

    added comments mostly

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@
 // what pieces do I need for the html????
 
 
-
+// Creating initial variables needed
 	const initialTime = 75;
 	var time = 75;
 	var score = 0;
@@ -33,9 +33,9 @@
 	var timeset;
 	var answers = document.querySelectorAll('#quizHolder button');
 
-
+// created an array containing objects of questions and answers to call
     var questions = [
-  
+            // this is the first object of this Array, or index 0
         {
         question: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
@@ -67,15 +67,16 @@
         },
     
     ];
-	  
+	//   this is getting any records if stored in localstorage
 	var recordsArray = [];
 	(localStorage.getItem('recordsArray')) ? recordsArray = JSON.parse(localStorage.getItem('recordsArray')): recordsArray = [];
 
-
+// function to call elements to make it easier
 	var queryElement = (element) => {
 		return document.querySelector(element);
 	}
 
+    // This hides and unhides elements
 	var onlyDisplaySection = (element) => {
 		var sections = document.querySelectorAll("section");
 		Array.from(sections).forEach((userItem) => {
@@ -83,7 +84,7 @@
 		});
 		queryElement(element).classList.remove('hide');
 	}
-
+        // this takes away the display of the score
 	var recordsHtmlReset = () => {
 		queryElement('#highScores div').innerHTML = "";
 		var i = 1;
@@ -100,7 +101,7 @@
 			answer.classList.remove('disable');
 		});
 	}
-
+        // this puts the questions and answers in the questionholder part
 	var setQuestionData = () => {
 		queryElement('#quizHolder p').innerHTML = questions[questionCount].question;
 		queryElement('#quizHolder button:nth-of-type(1)').innerHTML = `1. ${questions[questionCount].choices[0]}`;
@@ -109,6 +110,7 @@
 		queryElement('#quizHolder button:nth-of-type(4)').innerHTML = `4. ${questions[questionCount].choices[3]}`;
 	}
 
+        // this changes the question and tells if right or wrong
 	var quizUpdate = (answerCopy) => {
 		queryElement('#scoreIndicator p').innerHTML = answerCopy;
 		queryElement('#scoreIndicator').classList.remove('invisible', scoreIndicator());
@@ -116,7 +118,7 @@
 		{
 			answer.classList.add('disable');
 		});
-
+            // exits the quiz when the questions have been answered
 		setTimeout(() => {
 			if (questionCount === questions.length) {
 				onlyDisplaySection("#finish");
@@ -124,13 +126,14 @@
 				queryElement('#time').innerHTML = time;
 			} else {
 				setQuestionData();
+                // removes the disabled status
 				Array.from(answers).forEach(answer => {
 					answer.classList.remove('disable');
 				});
 			}
 		}, 1000);
 	}
-
+        // timer initially
 	var myTimer = () => {
 		if (time > 0) {
 			time = time - 1;
@@ -142,7 +145,7 @@
 		}
 	}
 
-
+        // on the start button, starts timer and quiz
 	var clock;
 	queryElement("#explain button").addEventListener("click", (e) => {
 		setQuestionData();
@@ -158,7 +161,7 @@
 		}, 1000);
 	}
 
-
+// makes an array of answers to compare, and tells what to do if correct or incorrect
 	Array.from(answers).forEach(check => {
 		check.addEventListener('click', function (event) {
 			if (this.innerHTML.substring(3, this.length) === questions[questionCount].answer) {
@@ -174,7 +177,7 @@
 		});
 	});
 
-
+// This makes an error if initials are more than 5
 	var errorIndicator = () => {
 		clearTimeout(timeset);
 		timeset = setTimeout(() => {
@@ -194,10 +197,12 @@
 			queryElement('#errorIndicator p').innerHTML = "Maximum of 5 characters allowed.";
 			queryElement('#errorIndicator').classList.remove('invisible', errorIndicator());
 		} else {
+            // this stores our array
 			recordsArray.push({
 				"initialRecord": initialsRecord,
 				"score": score
 			});
+            // this puts that info into local storage
 			localStorage.setItem('recordsArray', JSON.stringify(recordsArray));
 			queryElement('#highScores div').innerHTML = '';
 			onlyDisplaySection("#highScores");
@@ -206,13 +211,13 @@
 		}
 	});
 
-
+// clears scores
 	queryElement("#clearScores").addEventListener("click", () => {
 		recordsArray = [];
 		queryElement('#highScores div').innerHTML = "";
 		localStorage.removeItem('recordsArray');
 	});
-
+// lets you play again
 	queryElement("#reset").addEventListener("click", () => {
 		time = initialTime;
 		score = 0;
@@ -220,7 +225,7 @@
 		onlyDisplaySection("#explain");
 	});
 
-	
+	// shows high scores, but also exits the current quiz
 	queryElement("#scores").addEventListener("click", (e) => {
 		e.preventDefault();
 		clearInterval(clock);
